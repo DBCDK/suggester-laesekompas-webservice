f2da8ed004df42ee61ccaa7377fe15a2ff9fcc59
Noah Torp-Smith <nots@dbc.dk>
2019-12-04 13:05:11 +0100

    SE-2787-searcher-error: more comment updating

--- webservice/src/main/java/dk/dbc/laesekompas/suggester/webservice/solr/SuggestType.java
+++ webservice/src/main/java/dk/dbc/laesekompas/suggester/webservice/solr/SuggestType.java
@@ -4,12 +4,12 @@ package dk.dbc.laesekompas.suggester.webservice.solr;
  *
  * This is part of suggester-laesekompas-webservice
  *
- * microservice-sample is free software: you can redistribute it and/or modify
+ * suggester-laesekompas-webservice is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * microservice-sample is distributed in the hope that it will be useful,
+ * suggester-laesekompas-webservice is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
